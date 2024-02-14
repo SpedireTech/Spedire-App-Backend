a051002825e4b32214c5a5437d861b87d449c544
the_zen <alayandezainab64@gmail.com>
2024-02-14 13:47:55 +0100

    Project Structure Created

---
+++ src/main/java/com/example/spedire/SpedireApplication.java
@@ -0,0 +1,13 @@
+package com.example.spedire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpedireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpedireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spedire/SpedireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spedire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpedireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
