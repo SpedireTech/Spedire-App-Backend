3047893f27abaaf330d19be34c3e8d2b6387c9b3
the_zen <alayandezainab64@gmail.com>
2024-05-06 15:16:05 +0100

    commit

--- src/main/java/com/spedire/Spedire/security/GeneralConfig.java
+++ src/main/java/com/spedire/Spedire/security/GeneralConfig.java
@@ -3,12 +3,13 @@ package com.spedire.Spedire.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class GeneralConfig {
+public class GeneralConfig implements WebMvcConfigurer {
 
     @Bean
     public WebMvcConfigurer corsConfigurer(){
